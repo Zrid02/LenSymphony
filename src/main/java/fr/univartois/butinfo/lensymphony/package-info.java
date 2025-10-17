ab524d125b9549ca1b513bd7c7c9e1ada4622347
Romain Wallon <romain.wallon@univ-artois.fr>
2025-10-17 22:07:27 +0200

    :memo: Documentation for the main package.

---
+++ src/main/java/fr/univartois/butinfo/lensymphony/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The {@code fr.univartois.butinfo.lensymphony} package provides a library for
+ * generating musical notes and synthesizing music from, e.g., MusicXML files.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+
+package fr.univartois.butinfo.lensymphony;
