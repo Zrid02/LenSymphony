0f0a2b78dc2fa0f983367a2cd09f4901eee52903
Romain Wallon <romain.wallon@univ-artois.fr>
2025-10-17 22:05:37 +0200

    :sparkles: Interface for synthesizing notes.

---
+++ src/main/java/fr/univartois/butinfo/lensymphony/synthesizer/NoteSynthesizer.java
@@ -0,0 +1,54 @@
+/**
+ * LenSymphony - A simple music synthesizer library developed in Lens, France.
+ * Copyright (c) 2025 Romain Wallon - Université d'Artois.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package fr.univartois.butinfo.lensymphony.synthesizer;
+
+import fr.univartois.butinfo.lensymphony.notes.Note;
+
+/**
+ * The NoteSynthesizer interface defines a strategy for synthesizing audio samples from
+ * musical notes.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+public interface NoteSynthesizer {
+
+    /**
+     * The default audio sample rate in samples per second (Hz).
+     */
+    int SAMPLE_RATE = 44100;
+
+    /**
+     * Computes the audio samples for a given note.
+     *
+     * @param note The note to synthesize.
+     * @param tempo The tempo in beats per minute (BPM).
+     * @param volume The volume level for the note (0.0 to 1.0).
+     *
+     * @return An array of audio sample representing the synthesized note.
+     */
+    double[] synthesize(Note note, int tempo, double volume);
+
+}
