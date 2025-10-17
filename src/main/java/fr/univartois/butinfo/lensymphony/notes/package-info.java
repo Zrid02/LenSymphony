336a8ea2e692ffa39a5294a85d5a07096f05773b
Romain Wallon <romain.wallon@univ-artois.fr>
2025-10-17 22:04:56 +0200

    :memo: Documentation for the notes package.

---
+++ src/main/java/fr/univartois/butinfo/lensymphony/notes/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * The {@code fr.univartois.butinfo.lensymphony.notes} package contains the classes and
+ * interfaces representing different kinds of musical notes, in particular their pitch and
+ * duration.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+
+package fr.univartois.butinfo.lensymphony.notes;
