14ddfca905d589142574de7b698e4828408f4087
Romain Wallon <romain.wallon@univ-artois.fr>
2025-10-17 22:06:48 +0200

    :memo: Documentation for the synthesizer package.

---
+++ src/main/java/fr/univartois/butinfo/lensymphony/synthesizer/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The {@code fr.univartois.butinfo.lensymphony.synthesizer} package contains classes and
+ * interfaces related to the synthesis of musical sounds.
+ *
+ * @author Romain Wallon
+ *
+ * @version 0.1.0
+ */
+
+package fr.univartois.butinfo.lensymphony.synthesizer;
