07da71ace97d67c4581360702d4c559bacb70b48
antoine mouille <antoine_mouille@ens.univ-artois.fr>
2025-10-21 15:10:18 +0200

    representation des different instrument

--- src/main/java/fr/univartois/butinfo/lensymphony/notes/Instruments.java
+++ src/main/java/fr/univartois/butinfo/lensymphony/notes/Instruments.java
@@ -1,10 +1,24 @@
 package fr.univartois.butinfo.lensymphony.notes;
 
+import fr.univartois.butinfo.lensymphony.synthesizer.HarmonicSynthesizer;
+import fr.univartois.butinfo.lensymphony.synthesizer.NoteSynthesizer;
+import fr.univartois.butinfo.lensymphony.synthesizer.PureSound;
+
 public enum Instruments {
-	BASS_DRUM,
-	SNARE_DRUM,
-	CYMBAL,
-	TRIANGLE,
-	TIMPANI,
-	XYLOPHONE
+	BASS_DRUM(new PureSound()),
+	SNARE_DRUM(new HarmonicSynthesizer(new PureSound(),4)),
+	CYMBAL(new HarmonicSynthesizer(new PureSound(),5)),
+	TRIANGLE(new HarmonicSynthesizer(new PureSound(),6 )),
+	TIMPANI(new HarmonicSynthesizer(new PureSound(),7)),
+	XYLOPHONE(new HarmonicSynthesizer(new PureSound(),8));
+
+	private final NoteSynthesizer synthesizer;
+
+	Instruments(NoteSynthesizer synthesizer){
+		this.synthesizer = synthesizer;
+	}
+
+	public NoteSynthesizer getSynthesizer(){
+		return synthesizer;
+	}
 }
